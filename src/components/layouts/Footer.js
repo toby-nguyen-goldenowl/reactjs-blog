79069a4b8a93c56acdfdc8e64740cba970918fec
Toby <toby.nguyen.goldenowl@gmail.com>
2021-05-04 11:31:44 +0700

    fix:edit css

--- src/components/layouts/Footer.js
+++ src/components/layouts/Footer.js
@@ -5,7 +5,7 @@ const { Footer } = Layout;
 
 const FooterComponent = () => (
   <div className="footer">
-    <Footer style={{ textAlign: "center" }}>
+    <Footer className="content-footer" style={{ textAlign: "center" }}>
       DEV Community – A constructive and inclusive social network for software
       developers. With you every step of your journey. Built on Forem — the open
       source software that powers DEV and other inclusive communities. Made with
